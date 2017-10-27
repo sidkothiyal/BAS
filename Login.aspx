19a3a3184e941e8b1eb672d4e43b5343811ead74
sidkothiyal <sid.kothiyal27@gmail.com>
2017-10-27 16:19:41 +0530

    initial commit

---
+++ Login.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server"></asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="Label1" runat="server" Text="Username:"></asp:Label>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Password:"></asp:Label>
+    <asp:TextBox ID="TextBox2" runat="server" ></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Login" OnClick="Button1_Click" />
+    <asp:Label ID="Label3" runat="server" Text=""></asp:Label>
+</asp:Content>
+
---
+++ Login.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Control btn = Page.Master.FindControl("Button1");
+            btn.Visible = false;
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if(TextBox1.Text == "admin" && TextBox2.Text == "admin")
+        {
+            Session["login"] = "admin";
+        } else if (TextBox1.Text == "employee" && TextBox2.Text == "emp")
+        {
+            Session["login"] = "emp";
+        } else
+        {
+            Label3.Text = "Incorrect Credentials";
+        }
+
+
+    }
+}
